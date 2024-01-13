da8e9e992421e31c8368ccdecf7afddb4802b374
Debarun <paldebarun27@gmail.com>
2024-01-13 14:34:29 +0530

    all the names and descriptions af products are added

--- src/constants/Productdata.js
+++ src/constants/Productdata.js
@@ -64,12 +64,6 @@ const productdata=[
     name:"Amla",
  description: "Amla, also known as Indian gooseberry, is a small, green fruit native to the Indian subcontinent. Scientifically known as Phyllanthus emblica, it has been a staple in traditional Ayurvedic medicine for centuries. Amla is rich in vitamin C, antioxidants, and various phytochemicals. It is known for its potential health benefits, including boosting the immune system, promoting hair health, supporting digestion, and possessing anti-inflammatory properties. Amla is consumed in various forms, including fresh, dried, or as part of Ayurvedic formulations, and it is valued for its nutritional and medicinal properties."
 }
-,{
-
-    image:product6,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
-}
 ,
 {
 
@@ -88,8 +82,8 @@ const productdata=[
 {
 
     image:product8,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Gurbach",
+ description: "It is a monocot species of a flowering plant that is native to Eastern Asia and North America. Certain types of Vacha have also been found in the Himalayas and have been used widely in traditional medicines for their umpteen health benefitting properties.Vacha is a powerful medicinal herb imbued with immense therapeutic qualities, used extensively in the holistic science of Ayurvedic medicine since the times of Chakara and Sushruta.The name “Vacha” originated from Sanskrit, which means speaking clearly, as this herb works incredibly well to stimulate intelligence and expression.. Useful in stomach problems. Boosts Immune System. Useful in Digestion. Useful in Cough and cold."
 },
 
 {
@@ -102,121 +96,121 @@ const productdata=[
 {
 
     image:product10,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Bay leaf",
+ description: "The botanical name of Bay leaf is (Cinnamomumtamala). It is a small Mediterranean aromatic evergreen tree or large shrub with green, smooth, and hairless leaves. This herb is having a distinctive flavor and fragrance. It is easily found in the kitchen and commonly used in cooking. The dry leaves have full flavor but the fresh leaves have very mild and do not develop their full flavor. The leaves of this herb are used in dried form. This herb helps in pacifying the Vata and Kapha Dosha and also helps in increasing the Pitta Dosha."
 },
 
 {
 
     image:product11,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"kalpasi",
+ description: "Black Stone Flower is one of the most unusual spices in the Indian repertoire. It is a rare dried flower and a dominant spice in all Chettinad preparations. Black Stone Flower or Dagad Phool is a soft brown and black coloured lichen that gives the signature black color to various masalas like Goda Masala/Kala Masala. It has a strong earthy aroma and a very dry, light fluffy texture and feel to it. It is widely used in Chettinad cuisine and to some extent in Hyderabadi and Marathi cuisine. It is an edible lichen flora (a type of fungus), which grows on trees, rocks and stones. When used in small quantities, it imparts a strong woody aroma and flavor to the preparation. For better results it should be roasted in a little oil to release its full aroma."
 }
 ,
 
 {
 
     image:product12,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Dalchini",
+ description: "Bliss of Earth Stone Flowers also called as patthar ke phool or dagad phool in hindi is one of the most unusual spices in the indian repertoire. it is a rare dried flower and a dominant spice in all Chettinad preparations. Also known as kalpasi, stone flower is a soft brownish and black coloured lichen that gives the signature black color to various cuisine. It has a strong earthy atom and a very dry, light fluffy texture and fell to it. It is widely use in chettinad cuisine and to some extent in Hyderabadi and Marathi cuisine. Cinnamon, as most of the cinnamon in the global market is Cassia. Bliss of Earth helps cinnamon lovers to provide pure Ceylon Cinnamon, also known as true cinnamon which gives superior quality, taste and aroma while conditioning the better health of cinnamon lovers. bliss of Earth brings you the extraction of Mother earth from the hill country of Sri Lanka supporting small scale farmers while making our customers feel the perfection of ultimate taste and goodness of real ceylon cinnamon."
 },
 
 {
 
     image:product13,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Cinnamon stick",
+ description: "Cinnamon or Cinnamomum Zeylanicum is also popularly known as ‘Dalchini’ in Hindi. It is a dried bark of a small tree, which grows mainly in southern/western parts of India. Its leaves are commonly called ‘Tejpatra’ in Hindi. For ages, it has been one of the most significant spices available as part of ‘Garam Masala’ in Indian kitchens. It is available in the markets as rolled and dried sticks. Ayurveda defines cinnamon as ‘Tvak’. Cinnamon has a pungent and sweet taste, and it is hot in nature. It is considered beneficial in treating various conditions like oedema, flu, indigestion, cough etc., and thus, recommended for individuals having the ‘Kaphavata’ constitution. ."
 }
 ,
 
 {
 
     image:product14,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"jatamansi",
+ description: "Jatamansi, also known as “tapaswani” in Ayurveda, is a perennial, dwarf, hairy, herbaceous and endangered plant species. It acts as a brain tonic and helps to improve memory and brain functions by preventing cell damage due to its antioxidant property. It also calms down the brain and manages anxiety as well as insomnia.According to Ayurveda, Jatamansi helps in preventing wrinkles due to its Snigdha (oily) nature. It also promotes wound healing due to its Ropan (healing) property. Taking Jatamansi powder along with honey once or twice a day helps to improve memory functions. "
 },
 {
 
     image:product15,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Kapoor kachri",
+ description: "Also known as kapoor kachri, the kachri powder can promote hair growth and treat problems like dandruff and scalp infections. Because of its antioxidant and fortifying properties, kapoor kachri powder stimulates hair growth and strengthens hair follicles."
 }
 ,
 {
 
     image:product16,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Kutki",
+ description: "Kutaki is mainly used for liver disorders like jaundice as it protects the liver against cell damage caused by free radicals due to its antioxidant and hepatoprotective properties. This antioxidant property along with cardioprotective activity also helps to improve heart health by preventing damage to the heart.Kutaki powder along with honey can be taken twice a day to manage the symptoms of rheumatoid arthritis such as joint pain and inflammation due to its anti-inflammatory property.Gargling with Kutaki kwath (decoction) helps manage stomatitis (painful swelling inside the mouth) due to its Ropan (healing) property and Sita (nature)."
 }
 ,
 {
 
     image:product17,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Maida Lakdi",
+ description: "Ayurvedic herb, Maida Lakadi is Analgesic, Digestive, Chronic cough, Vaataj vikaar. The bark of Litsea glutinosa, is one of the most popular of native drugs, is considered to be capable of relieving pain, & good for stomach are considered to be mildly astringent, include treatment of diarrohea & dysentery."
 }
 ,
 {
 
     image:product18,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Pashanbhed",
+ description: "Pashanbhed or stone breaker is a popular Indian drug. It is also called stonebreaker as it can dissolve kidney stones. Pashanbhed is derived from the plant Bergenia ligulata and is used in the indigenous system of medicine."
 },
 {
 
     image:product19,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Pipli asli",
+ description: "Pippali is an effective herb in managing cough and cold. Pippali controls cough, releases mucus, clears air passages, thus allowing the patient to breathe freely. This is because Pippali has decongestant, bronchodilator and expectorant effects due to its Kapha balancing properties."
 },
 {
 
     image:product20,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Ramdana",
+ description: "Rajgira (Amaranth) or Ramadana, as it is popularly known in Northern India is a powerhouse of nutrients. This grain migrated to India from America and became an important part of our fasting ritual. Rajgira is a good source of calcium, protein and amino acids and is rich in iron, magnesium and Vitamin A, B and C too. This grain, which is only remembered during Navratri and Ganesh Chaturthi, should be included in our regular diet for the numerous benefits it offers."
 },
 {
 
     image:product21,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"manjishta",
+ description: "In ayurvedic medicine, Manjistha is a blood purifying herb. It cools and detoxifies the blood, removes stagnant blood, and dissolves obstructions in the blood flow. Manjistha also helps protect gums from receding. It is also believed to have astringent and anti-oxidant properties.Scientific studies have shown that it regulates blood pressure, blood vessel constriction, and helps protect from blood clot formation. Manjistha can also be used to treat uric acid and arthritis. Combined with other products, it can treat urinary infections, diarrhea, dysentery, and chronic fevers.Manjistha can be used to treat irregular menstruation. This herb can be used both internally and externally to bring luster to your skin and make it glow. Manjistha also helps removes pimples, freckles, other discolorations, and promotes the healing of skin tissues damaged by injury or infection."
 },
 {
 
     image:product22,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"satawari yellow",
+ description: "Shatavari is also known as Asparagus racemosus. It’s a member of the asparagus family. It’s also an adaptogenic herb. Adaptogenic herbs are said to help your body cope with physical and emotional stress.Shatavari is considered a general health tonic to improve vitality, making it a staple in ayurvedic medicine."
 },
 
 {
 
     image:product23,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Shikakai",
+ description: "Shikakai powder is a natural Ayurvedic herb used by Indian women for its delicate cleansing properties for highly sensitive scalps and for strengthening, volumizing and conditioning purposes. To be used alone or in addition to other dyeing or conditioning herbs, it will give body and shine to your hair."
 },
 {
 
     image:product24,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Soapnut",
+ description: "The soapnut contains the compound of saponin, which has natural cleansing properties, and therefore the soapnut can be used as a cleanser for hair, skin, and clothing. These saponins are also useful as insecticides, for purposes such as removing head lice off the scalp."
 }
 ,
 {
 
     image:product25,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Suhaga",
+ description: "Suhaga is known as Borax in English and Tankana in Ayurveda. It comes in crystalline form and has several characteristics that may help in the improvement of one’s health. It is also known as sodium tetraborate decahydrate and is made up of boric acid and sodium."
 },
 {
 
     image:product26,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Vaividang",
+ description: "Vidanga, commonly known as false black pepper, has various medicinal properties and is used in various ayurvedic formulations.Vidanga is generally used to expel worms and parasites from the stomach due to its anthelmintic properties. It is beneficial for indigestion and also helps to manage constipation due to its laxative property. Taking Vidanga churna regularly might help manage weight by lowering lipid levels and improving the metabolism of the body. It might also protect the heart against free cell damage by free radicals due to its cardioprotective and antioxidant activities. Vidanga might also be useful in managing depression by improving mood as well as brain functions due to its antidepressant activity."
 },
 {
 
     image:product27,
-    name:"Bekh Shoshan",
- description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+    name:"Vijaysar",
+ description: "Vijaysar is widely used in Ayurveda as a “Rasayana” (rejuvenating) herb. The bark of Vijaysar has significant importance in Ayurveda in managing diabetes and is also known as “The miracle cure for Diabetes” mainly due to its Tikta (bitter) nature.Vijaysar helps to manage blood sugar levels by preventing the damage to pancreatic cells and promoting insulin secretion due to its antioxidant and anti-inflammatory activity. Drinking water kept overnight in glasses made of Vijaysar wood is an age old practice for keeping blood sugar levels in control. Taking 1-2 Vijaysar capsules twice a day is also good for diabetes and weight loss."
 }
 
 ]
