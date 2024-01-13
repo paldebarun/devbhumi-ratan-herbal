9a0ef8e5f98511126a22bfd406c428d55fa84f6e
Debarun <paldebarun27@gmail.com>
2024-01-13 12:39:33 +0530

    new products are added

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,7 +21,7 @@ const Product = ({image,description,product}) => {
   return (
     <div onClick={handleProductOnclickHandler}  className='hover:scale-105 m-5  hover:cursor-pointer transition-all duration-200  py-5 flex flex-col  w-full md:w-[300px] lg:w-[350px] justify-between shadow-lg gap-[20px] h-[660px] sm:h-[700px] items-center'>
     
-    <img src={product.image} className=' w-[200px] sm:w-full h-auto  ' />
+    <img src={product.image} className=' w-[200px] h-[250px] sm:w-full   ' />
     
     <div className='w-full h-auto flex justify-center items-center'>
         <div className='w-3/12 h-[5px] bg-green-700 rounded-full'>
@@ -29,7 +29,7 @@ const Product = ({image,description,product}) => {
         </div>
     </div>
     
-    <div className='font-bold text-green-700 text-3xl sm:text-2xl '>{product.name}</div>
+    <div className='font-bold text-green-700 text-3xl sm:text-xl lg:text-2xl '>{product.name}</div>
 
     <div className='w-full  text-center py-5 px-5 text-slate-500'>
 
--- src/constants/Productdata.js
+++ src/constants/Productdata.js
@@ -4,50 +4,219 @@ import product3 from '../images/product3.png'
 import product4 from '../images/product4.png'
 import product5 from '../images/product5.png'
 import product6 from '../images/product6.png'
+import product7 from '../images/product7.png'
+import product8 from '../images/product8.png'
+import product9 from '../images/product9.png'
+import product10 from '../images/product10.png'
+import product11 from '../images/product11.webp'
+import product12 from '../images/product12.jpg'
+import product13 from '../images/product13.jpg'
+import product14 from '../images/product14.jpg'
+import product15 from '../images/product15.jpg'
+import product16 from '../images/product16.png'
+import product17 from '../images/product17.webp'
+import product18 from '../images/product18.jpg'
+import product19 from '../images/product19.jpg'
+import product20 from '../images/product20.webp'
+import product21 from '../images/product21.jpg'
+import product22 from '../images/product22.jpg'
+import product23 from '../images/product23.jpeg'
+import product24 from '../images/product24.jpg'
+import product25 from '../images/product25.jpg'
+import product26 from '../images/product26.webp'
+import product27 from '../images/product27.jpg'
 
 
 const productdata=[
 
 {
  image:product1,
- name:"spice",
- description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+ name:"Daruharidra",
+ description: "Daruharidra, Daru Haldi, Indian barberry, Tree turmeric, and Chitra are some common names for Berberis aristata. Daruhidra belongs to the Berberidaceae family and is a spiny, hard, and yellowish herb.Almost every portion of this plant has considerable medical significance according to traditional Indian and Chinese medicinal traditions. Many traditional ayurvedic remedies use its roots, such as Darvyadi kvatha, Rasaut, Darvyadi leha, Rasanjana, Dasanga lepa, Darvyadi taila, and others; bark, rhizomes, stem, leaves, and fruits. Pharmaceuticals, nutraceuticals, and cosmeceutical products use the Berberis aristata plant extracts. The plant includes several significant phytochemicals including proto-berberine, isoquinoline, bisbenzyl-isoquinoline, and other bioactive constituents like flavonoids and phenolic acids."
 
 
 },
 
 {
     image:product2,
-    name:"spice",
- description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+    name:"Atis Mithi",
+ description: "We provide Authentic Herbs, prepared according to the GMP guidelines. Exceptional consideration is taken to utilize the new and pure ingredients, the herbs are appropriately scrubbed and handled according to the customary techniques to hold its regular smell, shading, taste and viability and keeping up the quality."
 
 },
 
 {
   
     image:product3,
-    name:"spice",
- description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+    name:"Chirata",
+ description: "Swertia Chirayita, also known as Chirata is a herb that is traditionally used to treat a variety of conditions like malaria, diabetes, and liver disorders.Chirata contains several compounds that contribute to its medicinal value. The compounds include Xanthones, alkaloids, and glycosides. They also consist of ophelic acid, chiratin, steric acid, oleic acid, and palmitic acid. Swertanone, amarogenin, and chiratol are other important components present in Chirata."
 
 },
 
 {
     image:product4,
-    name:"spice",
-    description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+    name:"Yuvika Amalved",
+    description: "Yuvika Authentic Herbs, seeds and Nuts are processed as per the GMP standards. Special care is taken to use the fresh and pure ingredients, the herbs are properly cleansed and processed as per the traditional / authoritative texts / methods to retain its natural aroma, color, taste, purity and effectiveness and maintaining the quality. IndianJadiBooti Products Contains No Added Colors, Flavors or Preservatives. Benefits: There are no substitutes in modern medicine for the Fresh and Pure Natural Organic Herbs. Along with the presence of nutrients, they serve numerous health benefits and possess an earthy taste. Storage Information: Store in a cool and Dry place away from Sunlight and Moisture. Keep it in Airtight Container after Opening. Allergen Information: Packed in a facility that also handles Tree Nuts and Cereals."
 
 },
 
 {
     image:product5,
-    name:"spice",
- description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+    name:"Amla",
+ description: "Amla, also known as Indian gooseberry, is a small, green fruit native to the Indian subcontinent. Scientifically known as Phyllanthus emblica, it has been a staple in traditional Ayurvedic medicine for centuries. Amla is rich in vitamin C, antioxidants, and various phytochemicals. It is known for its potential health benefits, including boosting the immune system, promoting hair health, supporting digestion, and possessing anti-inflammatory properties. Amla is consumed in various forms, including fresh, dried, or as part of Ayurvedic formulations, and it is valued for its nutritional and medicinal properties."
 }
 ,{
 
     image:product6,
-    name:"spice",
- description: "Cardamom, often referred to as the \"Queen of Spices,\" is a fragrant and versatile spice originating from the tropical regions of South India. Known for its distinctive sweet, spicy, and slightly floral aroma, cardamom has been prized in both culinary and medicinal practices for centuries. Whether used in aromatic coffees, savory dishes, or sweet desserts, its unique flavor profile adds depth and complexity, making cardamom an essential ingredient in global cuisines. Additionally, its potential health benefits, including digestive support and antioxidant properties, further elevate its esteemed status in the world of spices."
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+{
+
+    image:product9,
+    name:"Devdaru gulia",
+ description: "Devdaru also called as Deodar or Himalayan Cedar is popular known as the Wood of Gods. All parts of this plant are used for various medicinal benefits.Devdaru helps to relieve cough by removing mucus from the respiratory tract due to its expectorant property. It might also be beneficial in managing asthma by increasing the movements of the respiratory tract due to its antispasmodic activity. Devdaru might be good for diabetics as it helps manage the blood sugar levels by stimulating insulin secretion. It also helps to calm the mind and improve sleep pattern thereby managing insomnia.Devdaru oil obtained from Devdaru trees has many benefits. This oil can be applied on the body to manage fever by increasing sweating due to its diaphoretic sweat-inducing property."
+},
+
+{
+
+    image:product7,
+    name:"Curry leaves",
+ description: "Curry leaves, scientifically known as Murraya koenigii, are aromatic leaves commonly used as a seasoning in various cuisines, particularly in South Asian cooking.n Ayurveda, the ancient Indian system of medicine, curry leaves (Murraya koenigii) are highly regarded for their diverse therapeutic properties. Known as Kadi Patta in Ayurvedic tradition, these leaves are valued for their ability to balance the doshas, particularly pacifying excess Pitta and Kapha. Rich in antioxidants, curry leaves are believed to detoxify the body, purify the blood, and support overall digestive health. Ayurveda attributes these leaves with anti-inflammatory properties, aiding in conditions such as indigestion and nausea. Additionally, curry leaves are thought to enhance hair health, addressing issues like dandruff and promoting lustrous hair growth. Regular consumption or topical application of curry leaves is recommended in Ayurvedic practices to harness their holistic benefits and contribute to both physical well-being and vitality."
+}
+,
+{
+
+    image:product8,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+
+{
+
+    image:product6,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+
+{
+
+    image:product10,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+
+{
+
+    image:product11,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+
+{
+
+    image:product12,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+
+{
+
+    image:product13,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+
+{
+
+    image:product14,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product15,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+{
+
+    image:product16,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+{
+
+    image:product17,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+{
+
+    image:product18,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product19,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product20,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product21,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product22,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+
+{
+
+    image:product23,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product24,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+}
+,
+{
+
+    image:product25,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product26,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
+},
+{
+
+    image:product27,
+    name:"Bekh Shoshan",
+ description: "Yuvika Bekh Shoshan - Begshoshan is an ayurvedic remedy which helps to reduce the weakness of the muscles. It helps to reduce pain in the ears, neck, back and head. It is effective in case of facial spasms and facial contractures. It may be helpful in relieving facial paralysis."
 }
 
 ]
